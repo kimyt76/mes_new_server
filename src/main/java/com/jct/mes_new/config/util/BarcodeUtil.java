7c749d9f6f5c71ce2b7a1d20c6e094ecdca974c0
kimyt <kimyt@jincostech.com>
2025-11-12 08:25:19 +0900

    바코드 기능 추가

---
+++ src/main/java/com/jct/mes_new/config/util/BarcodeUtil.java
@@ -0,0 +1,17 @@
+package com.jct.mes_new.config.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+
+public class BarcodeUtil {
+
+    private BufferedImage generateQrImage(String text) throws Exception {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+}
