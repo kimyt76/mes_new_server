0034d8045d0d9f000c8e88d0ead9c6643f1de048
kimyt <kimyt@jincostech.com>
2026-01-23 15:14:41 +0900

    칭량기본, 처방 리스트 vo 신규

---
+++ src/main/java/com/jct/mes_new/biz/proc/vo/WeighInfoVo.java
@@ -0,0 +1,12 @@
+package com.jct.mes_new.biz.proc.vo;
+
+import com.jct.mes_new.biz.lab.vo.BomRecipeVo;
+import lombok.Data;
+
+@Data
+public class WeighInfoVo {
+
+    private ProcWeighVo procWeighVo;
+    private BomRecipeVo bomRecipeVo;
+
+}
