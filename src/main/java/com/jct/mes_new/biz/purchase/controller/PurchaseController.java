c12e9ef338409d6933640d81972f4c147c22c1d6
kimyt <kimyt@jincostech.com>
2026-03-09 17:12:09 +0900

    구매 인쇄 추가

--- src/main/java/com/jct/mes_new/biz/purchase/controller/PurchaseController.java
+++ src/main/java/com/jct/mes_new/biz/purchase/controller/PurchaseController.java
@@ -11,12 +11,21 @@ import com.jct.mes_new.biz.purchase.vo.PurchaseRequestVo;
 import com.jct.mes_new.biz.purchase.vo.PurchaseVo;
 import com.jct.mes_new.config.common.ApiResponse;
 import com.jct.mes_new.config.common.MessageUtil;
+import com.jct.mes_new.config.util.JasperUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -74,7 +83,7 @@ public class PurchaseController {
 
     /**
      * 구매 삭제
-     * @param vo
+     * @param purId
      * @return
      */
     @GetMapping("/deletePurchase/{id}")
@@ -83,7 +92,44 @@ public class PurchaseController {
         return ResponseEntity.ok(ApiResponse.ok(messageUtil.get("success.deleted")));
     }
 
+    /**
+     * 구매 인쇄
+     * @param vo
+     * @return
+     * @throws Exception
+     */
+    @PostMapping(value = "/printOut")
+    public ResponseEntity<Resource> printOut(@RequestBody PurchaseVo vo) throws Exception {
+        List<PurchaseVo.searchPurchaseListVo> purList = getPurchaseList(vo);
+        List<Map<String, Object>> list = new ArrayList<>();
 
+        try {
+
+            for(PurchaseVo.searchPurchaseListVo pur : purList) {
+                Map<String,Object> map = new HashMap<>();
+
+                map.put("transNo", pur.getPurDateSeq());
+                map.put("customerName", pur.getCustomerName());
+                map.put("itemName", pur.getItemName());
+                map.put("sumQty", pur.getTotQty());
+                map.put("sumSupplyAmt", pur.getTotPrice());
+                map.put("srcStorageName", pur.getStorageName());
+                map.put("orderType", pur.getOrderType());
+                map.put("erpYn", "Y" );
+                map.put("memberName", pur.getManagerName());
+                list.add(map);
+            }
+            InputStream reportStream = getClass().getResourceAsStream("/report/mat_in_list.jrxml");
+            byte[] pdfContent = JasperUtil.getPdfBinary(reportStream, list);
+            ByteArrayResource resource = new ByteArrayResource(pdfContent);
+            HttpHeaders header = JasperUtil.getHeader("mat_in_list", pdfContent.length);
+
+            return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new Exception("리포트 생성중 에러발생!");
+        }
+    }
 
 
 }
